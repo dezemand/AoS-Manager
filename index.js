8d53af91e614410bcdc25b4043da5ff14d495351
netraameht <maarten@v-ittersum.nl>
2015-02-08 17:40:51 -0500

    Moved functions to lib.js (aoslib)

--- index.js
+++ index.js
@@ -3,6 +3,7 @@ var httplib = require('http');
 var http = require('http').Server(app);
 var io = require('socket.io')(http);
 var fs = require("fs");
+var aoslib = require("./aoslib");
 var servers = []; 
 getServerIDs(function (servids) {servers = servids});
 
@@ -49,48 +50,3 @@ io.on('connection', function (socket) {
     }
   });
 });
-
-function getID(serv, callback) {
-  httplib.get("http://services.buildandshoot.com/getip", function (res) {
-    var body = '';
-    res.on('data', function(d) {body += d;});
-    res.on('end', function() {
-      var a = parseInt(body.split(".")[0]);
-      var b = parseInt(body.split(".")[1]) * Math.pow(2, 8);
-      var c = parseInt(body.split(".")[2]) * Math.pow(2, 16);
-      var d = parseInt(body.split(".")[3]) * Math.pow(2, 24);
-      var identifier = a + b + c + d;
-      callback(serv, identifier);
-    });
-  });
-}
-
-function getConfig(serv, callback) {
-  fs.readFile("./servers/"+serv+"/feature_server/config.txt", "utf8", function(err, config) {
-    config = JSON.parse(config);
-    callback(serv, config);
-  });
-}
-
-function getStatus(serv, port, callback) {
-  httplib.get({host:'localhost',port:port,path:'/'}, function (res) {
-    var body = '';
-    res.on('data', function(d) {body += d;});
-    res.on('end', function() {
-      var map = body.split("The server is currently running ")[body.split("The server is currently running ").length-1].split(".</p>")[0].split(" by ");
-      var players = body.split("Current players: ")[body.split("Current players: ").length-1].split("/")[0];
-      callback(serv, map, players);
-    });
-  });
-}
-
-function getServerIDs(callback) {
-  var servers = [];
-  fs.readdir("./servers/", function(err, files) {
-    for(i=0; i<files.length;i++) {
-      if(fs.statSync("./servers/"+files[i]).isDirectory())
-        servers.push(files[i]);
-    }
-    callback(servers);
-  });
-}
