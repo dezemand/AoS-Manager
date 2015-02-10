558bfa6ea29b8635b1ba8aa9d12a164234bd5fd3
netraameht <maarten@v-ittersum.nl>
2015-02-10 11:07:27 -0500

    Changed path back

--- bin/start_server.sh
+++ bin/start_server.sh
@@ -1,10 +1,10 @@
 #!/bin/bash
 SERVER=$1
 
-if [ -f "../servers/$SERVER/run_server.sh" ]; then
+if [ -f "./servers/$SERVER/run_server.sh" ]; then
    echo "Starting server $SERVER"
-   mv ../logs/$SERVER.log logs/$SERVER.log.1
-   cd ../servers/$SERVER/
+   mv ./logs/$SERVER.log logs/$SERVER.log.1
+   cd ./servers/$SERVER/
    sh run_server.sh | tee -a ../../logs/$SERVER.log
 else
    echo "Server $SERVER does not exist."
